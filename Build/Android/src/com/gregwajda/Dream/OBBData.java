0ee6f1dfd848617704996b4a4c99b609108d3e0d
Greg Wajda <greg.wajda@comcast.net>
2015-09-24 16:59:49 +0200

    Fixed Difficulty Buttons
    Buttons now highlight which difficulty you are currently on.

--- Build/Android/src/com/gregwajda/Dream/OBBData.java
+++ Build/Android/src/com/gregwajda/Dream/OBBData.java
@@ -16,8 +16,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"3", // the version of the APK that the file was uploaded against
-17970699 // the length of the file in bytes
+"5", // the version of the APK that the file was uploaded against
+17994473 // the length of the file in bytes
 )
 };
 };
